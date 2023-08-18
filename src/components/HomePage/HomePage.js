950b21f3ce88b9d2571a8e639937aff8864d645b
joyumoekpo <joyumoekpo1@gmail.com>
2023-08-18 11:44:26 -0500

    Linked cover image

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./HomePage.css";
+import { elephantCover } from "../../assets/elephant--homepage-cover.jpg";
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			HomePage
+			<img src={elephantCover} alt="Homepage Cover" />
+		</div>
+	);
 };
 
 export default HomePage;
