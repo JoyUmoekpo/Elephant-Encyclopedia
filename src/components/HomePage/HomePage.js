140c20ee6035df0ea6a1679f1b3b064b28b8f9b6
joyumoekpo <joyumoekpo1@gmail.com>
2023-08-18 12:21:43 -0500

    Sized cover image

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,7 +5,8 @@ import elephantCover from "../../assets/elephant--homepage-cover.jpg";
 const HomePage = () => {
 	return (
 		<div>
-			<img src={elephantCover} alt="Homepage Cover" />
+			{" "}
+			<img src={elephantCover} alt="Homepage Cover" className="cover" />{" "}
 		</div>
 	);
 };
