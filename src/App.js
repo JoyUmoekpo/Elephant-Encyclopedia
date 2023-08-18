f53349462f9f057da44b887869fe261e93a31669
joyumoekpo <joyumoekpo1@gmail.com>
2023-08-18 11:48:34 -0500

    Linked HomePage Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
 
 const App = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>Elephants🐘!</h1>
+				<HomePage />
 			</header>
 		</div>
 	);
