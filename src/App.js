d423ac2a2f9e17c52ff5039801e24f70626e6b14
joyumoekpo <joyumoekpo1@gmail.com>
2024-05-25 12:01:50 -0500

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
 			greeting: "Hello everyone!",
 			explanation:
 				"This is my application, Elephant Encyclopedia! Here you can fins all kinds of information about elephants and their habits.",
-			message: "Elephants🐘 I can't wait to get out of here",
+			message: "Elephants🐘",
 		},
 	];
 
